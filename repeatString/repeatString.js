3fa533bb348a8c09cee59fb51ded19b6713e5074
Joseph Dorsey <jmdorsey92@gmail.com>
2021-04-12 20:04:57 -0700

    prog(repeatString.js) finished all 7 challenges 🚀

---
+++ repeatString/repeatString.js
@@ -0,0 +1,45 @@
+const repeatString = function (x, y) {
+  // Test - 1
+  //  let repeatedString = "";
+  // prints heyheyhey 3 times
+  //   for (let i = 0; i < 3; i++) {
+  // repeatedString += string;
+  // }
+  // return repeatedString;
+  // Test 2 - prints heyheyhey 10 times
+  //   for (let i = 0; i < 10; i++) {
+  //     repeatedString += string;
+  //   }
+  //   return repeatedString;
+  // Test 3 - repeat 1 time
+  //   let i = 0;
+  //   do {
+  //     repeatedString += string;
+  //     i++;
+  //   } while (i < 1);
+  //   return repeatedString;
+  //Test 4 - repeat 0 times - make it say "";
+  //   let i = 0;
+  //   do {
+  //     repeatedString += string;
+  //     i++;
+  //   } while (i < 0);
+  //   return "";
+  // Test 5 Return error with negative numbers
+  //   for (let i = -1; i < 5; i++) {
+  //     return "ERROR";
+  //   }
+  // Test 6 - repeat string number random times
+  //   let number = Math.random();
+  //   for (let i = 0; i < number; i++) {
+  //     return x.repeat(y);
+  //   }
+  // Test 7 - work with blank strings
+  //   for (let i = 0; i < 5; i++) {
+  //     if (x === "") {
+  //       return x.repeat(y);
+  //     }
+  //   }
+};
+
+module.exports = repeatString;
