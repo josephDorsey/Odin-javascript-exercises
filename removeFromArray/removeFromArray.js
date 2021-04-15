e840bbe22faef82e469d7eee8fe699096316c5b7
Joseph Dorsey <jmdorsey92@gmail.com>
2021-04-15 10:14:33 -0700

    prog(removeFromArray.js) completed Test#2 - removing multiple items from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,12 +1,22 @@
+// jasmine removeFromArray.spec.js
 const removeFromArray = function (x) {
-  // Test 1 remove single value
+  // Test 1 - remove single value
+  // for (let i = 0; i < x.length; i++) {
+  //   if (x[i] === 3) {
+  //     x.splice(i, 1); // this tells how many elements to remove
+  //     i--;
+  //     return x;
+  //   }
+  // }
+
+  // Test 2 - remove multiple values
   for (let i = 0; i < x.length; i++) {
-    if (x[i] === 3) {
-      x.splice(i, 1);
+    if (x[i] === 2 || x[i] === 3) {
+      x.splice(i, 2); // this tells to remove 2 elements
       i--;
       return x;
     }
   }
 };
-
+// console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 module.exports = removeFromArray;
